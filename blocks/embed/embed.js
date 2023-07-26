101c0b0b3a66966dab3d8660ec9b1672b5c0ff5d
jamesjtalbot <jtalbot@adobe.com>
2023-07-26 13:21:08 -0700

    update
    update

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -1,3 +1,8 @@
+/*
+ * Embed Block
+ * Show videos and social posts directly on your page
+ * https://www.hlx.live/developer/block-collection/embed
+ */
 
 const loadScript = (url, callback, type) => {
   const head = document.querySelector('head');
@@ -36,7 +41,8 @@ const embedVimeo = (url, autoplay) => {
   const [, video] = url.pathname.split('/');
   const suffix = autoplay ? '?muted=1&autoplay=1' : '';
   const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-      <iframe src=iframe src="https://player.vimeo.com/video/${video}${suffix}"  style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+      <iframe src="https://player.vimeo.com/video/${video}${suffix}" 
+      style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
       frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen  
       title="Content from Vimeo" loading="lazy"></iframe>
     </div>`;
