400ebd7fdea9ccc85dc61ce34ce8f15185e3c5f6
jamesjtalbot <jtalbot@adobe.com>
2023-07-26 13:05:13 -0700

    update
    update

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -36,9 +36,9 @@ const embedVimeo = (url, autoplay) => {
   const [, video] = url.pathname.split('/');
   const suffix = autoplay ? '?muted=1&autoplay=1' : '';
   alert ({video});
-  alert ({suffix});
+  alert (${suffix});
   const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-      <iframe src="https://player.vimeo.com/video/537322975/" 
+      <iframe src="https://player.vimeo.com/video/${video}${suffix}"; 
       style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
       frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen  
       title="Content from Vimeo" loading="lazy"></iframe>
