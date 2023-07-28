2f8f3838f6e2d972a066d7ecf9c363861b3a550e
vmitra <vmitra@adobe.com>
2023-07-28 16:05:21 -0400

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -38,6 +38,65 @@ function toggleMenu(nav, mobileNav, forceExpanded = null) {
   }
 }
 
+function decorateNav(html) {
+  const nav = document.createElement('nav');
+  nav.id = 'nav';
+  nav.innerHTML = html;
+  const dNav = nav.querySelector('.navigation');
+  const sideNav = nav.querySelector('.sidenav');
+  const mobileNav = document.createElement('div');
+  mobileNav.className = 'hamburger-mobile';
+  if (dNav) {
+    dNav.querySelectorAll(':scope > div > div > ul > li').forEach((navitem) => {
+      if (navitem.innerText.includes(':')) {
+        navitem.classList.add('button');
+        navitem.innerText = navitem.innerText.replace(':', '');
+      }
+      navitem.addEventListener('click', () => {
+        if (isDesktop.matches) {
+          const expanded = navitem.getAttribute('aria-expanded') === 'true';
+          navitem.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+        }
+      });
+    });
+  }
+  // hamburger for mobile
+  const hamburger = document.createElement('div');
+  hamburger.classList.add('nav-hamburger');
+  hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
+    <span class="nav-hamburger-icon"></span>
+    </button>`;
+  if (sideNav) {
+    if (isMobile.matches) {
+      mobileNav.appendChild(sideNav.querySelector(':scope > div > div > ul'));
+      dNav.parentNode.appendChild(mobileNav);
+      hamburger.addEventListener('click', () => toggleMenu(nav, mobileNav));
+    } else {
+      sideNav.querySelectorAll(':scope > div > div').forEach((div) => {
+        const bgImage = div.querySelector('img');
+        if (bgImage) {
+          div.className = 'sidenav-left image-appear-animation';
+          div.style.backgroundImage = `url('${bgImage.src}')`;
+          sideNav.querySelector('picture').remove();
+          const divText = document.createElement('div');
+          divText.className = 'text-left';
+          sideNav.querySelectorAll('p, h3, hr').forEach((item) => {
+            divText.append(item);
+          });
+          div.appendChild(divText);
+        } else div.className = 'sidenav-right';
+      });
+      dNav.parentNode.appendChild(sideNav);
+      hamburger.addEventListener('click', () => toggleMenu(nav, sideNav));
+    }
+  }
+  dNav.parentNode.appendChild(hamburger);
+  nav.setAttribute('aria-expanded', 'false');
+  // prevent mobile nav behavior on window resize
+  decorateIcons(nav);
+  return nav;
+}
+
 /**
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
@@ -47,69 +106,23 @@ export default async function decorate(block) {
   const navMeta = getMetadata('nav');
   const navPath = navMeta ? new URL(navMeta).pathname : '/nav';
   const resp = await fetch(`${navPath}.plain.html`);
-
   if (resp.ok) {
     const html = await resp.text();
-
     // decorate nav DOM
-    const nav = document.createElement('nav');
-    nav.id = 'nav';
-    nav.innerHTML = html;
-    const dNav = nav.querySelector('.navigation');
-    const sideNav = nav.querySelector('.sidenav');
-    const mobileNav = document.createElement('div');
-    mobileNav.className = 'hamburger-mobile';
-    if (dNav) {
-      dNav.querySelectorAll(':scope > div > div > ul > li').forEach((navitem) => {
-        if (navitem.innerText.includes(':')) {
-          navitem.classList.add('button');
-          navitem.innerText = navitem.innerText.replace(':', '');
-        }
-        navitem.addEventListener('click', () => {
-          if (isDesktop.matches) {
-            const expanded = navitem.getAttribute('aria-expanded') === 'true';
-            navitem.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-          }
-        });
-      });
-    }
-    // hamburger for mobile
-    const hamburger = document.createElement('div');
-    hamburger.classList.add('nav-hamburger');
-    hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
-      <span class="nav-hamburger-icon"></span>
-      </button>`;
-    if (sideNav) {
-      if (isMobile.matches) {
-        mobileNav.appendChild(sideNav.querySelector(':scope > div > div > ul'));
-        dNav.parentNode.appendChild(mobileNav);
-        hamburger.addEventListener('click', () => toggleMenu(nav, mobileNav));
-      } else {
-        sideNav.querySelectorAll(':scope > div > div').forEach((div) => {
-          const bgImage = div.querySelector('img');
-          if (bgImage) {
-            div.className = 'sidenav-left image-appear-animation';
-            div.style.backgroundImage = `url('${bgImage.src}')`;
-            sideNav.querySelector('picture').remove();
-            const divText = document.createElement('div');
-            divText.className = 'text-left';
-            sideNav.querySelectorAll('p, h3, hr').forEach((item) => {
-              divText.append(item);
-            });
-            div.appendChild(divText);
-          } else div.className = 'sidenav-right';
-        });
-        dNav.parentNode.appendChild(sideNav);
-        hamburger.addEventListener('click', () => toggleMenu(nav, sideNav));
-      }
-    }
-    dNav.parentNode.appendChild(hamburger);
-    nav.setAttribute('aria-expanded', 'false');
-    // prevent mobile nav behavior on window resize
-    decorateIcons(nav);
+    const nav = decorateNav(html);
     const navWrapper = document.createElement('div');
     navWrapper.className = 'nav-wrapper';
     navWrapper.append(nav);
     block.append(navWrapper);
+    window.matchMedia('(min-width: 768px)').addEventListener('change', (mediaQuery) => {
+      if (mediaQuery.matches) {
+        document.location.reload();
+      }
+    });
+    window.matchMedia('(max-width: 767px)').addEventListener('change', (mediaQuery) => {
+      if (mediaQuery.matches) {
+        document.location.reload();
+      }
+    });
   }
 }
