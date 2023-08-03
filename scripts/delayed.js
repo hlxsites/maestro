079982984440530f7ff6cd61fd12ea181a24f825
Kunwar <kunwarsaluja@hotmail.com>
2023-08-03 10:27:29 -0400

    load gtm script

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,16 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './lib-franklin.js';
+import { fetchPlaceholders, sampleRUM } from './lib-franklin.js';
+
+const placeholders = await fetchPlaceholders();
+
+function loadGoogleTagManager() {
+  // google tag manager
+  const { gtmId } = placeholders;
+  // eslint-disable-next-line
+  (function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, 'script', 'dataLayer', gtmId);
+}
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-// add more delayed functionality here
+loadGoogleTagManager();
